305fb93c71cd43627628e66bc6da8fc02444e984
khechinimed <khechinibakr20@gmail.com>
2023-11-15 15:15:14 +0100

    Home and login page init

---
+++ terrasafe-front/src/pages/home.jsx
@@ -0,0 +1,12 @@
+import React, { PureComponent } from 'react'
+
+export default class home extends PureComponent {
+  render() {
+    return (
+      <div className='home'>
+        <h1>TerraSafe</h1>
+        <p>Home Page</p>
+      </div>
+    )
+  }
+}
---
+++ terrasafe-front/src/pages/login.jsx
@@ -0,0 +1,9 @@
+import React, { PureComponent } from 'react'
+
+export default class login extends PureComponent {
+  render() {
+    return (
+      <div>Login Page</div>
+    )
+  }
+}
